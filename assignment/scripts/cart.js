2fd274d5800eb19652f3f18b7b9c4f3614de830a
Mai Hlee <vangmaihlee@ymail.com>
2022-05-29 01:39:41 -0500

    completed

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -1,3 +1,40 @@
 console.log('***** Cart Functions *****');
 // Make sure to test all functions here in the JS file!
 // We want to see how you are testing your code!!!
+
+console.log( '--Adding items--');
+
+let basket =[];
+
+function addItem(itemOne, itemTwo){
+    basket.push('fries','ketchup');
+    return true;
+}
+
+console.log('First confirmation:', addItem('fries','ketchup'));
+console.log(`Items in basket: ${basket}`);
+console.log('Number of items:', basket.length);
+
+
+console.log( '--Loop & logging--');
+
+function listItems(){
+    for (let i=0; i < basket.length; i++) {
+        console.log(basket[i])
+    }
+}
+
+listItems();
+//console.log('Items on list:', listItems());
+
+
+
+console.log( '--Empty Array--');
+
+function empty(){
+    return basket.length = 0;
+}
+console.log(empty());
+console.log('Number of items now are:', basket.length);
+
+
